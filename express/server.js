33d30eef05f60457a4566ebabf42e3891fe0d798
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-24 12:15:08 -0600

    add adjustments to knexfile, knex.js, and server.js for databse connection

--- express/server.js
+++ express/server.js
@@ -1,17 +1,23 @@
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
+const knex = require('./db/knex')
 const PORT = 8080;
 
 // Express Configuration
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
+App.use(Express.urlencoded({ extended: false }));
+App.use(Express.json());
 App.use(Express.static('public'));
 
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
+
+// can we use routers instead or with this
+// app.get('/tasks', (req, res) => {
+//   // use the knex variable above to create dynamic queries
+// });
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
