1cf8aa32a4f26e61f1525c85dbf995b14005f602
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-25 18:59:26 -0600

    update migrations second round

--- express/db/migrations/20210926000349_jobs.js
+++ express/db/migrations/20210926000349_jobs.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('jobs', table => {
     table.increments('id') // this represents the primary key.
-    table.foreign('crew_id').references('id').inTable('crews') // this is a column.
-    table.foreign('contract_id').references('id').inTable('contracts')
+    table.foreign('id', 'crew_id').references('id').inTable('crews') // this is a column.
+    table.foreign('id', 'contract_id').references('id').inTable('contracts')
     table.date('date')
     table.time('start_time')
     table.time('end_time')
