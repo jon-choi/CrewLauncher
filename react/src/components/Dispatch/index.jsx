1989cdcf1d47c9f9e6a5ea4606c3f1cd01fe01cf
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-27 16:50:45 -0600

    add components to jsx of dispatch index, add empty functions to dispatchDataHelper

--- react/src/components/Dispatch/index.jsx
+++ react/src/components/Dispatch/index.jsx
@@ -1,30 +1,55 @@
 
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
-// import Dashboard from './Dashboard';
-// import ClientsPage from './ClientsPage';
-// import ContractsPage from './ContractsPage';
-// import ContractForm from './ContractForm';
-// import JobForm from './JobForm';
-// import PackageForm from './PackageForm';
-// import Navigation from './Navigation';
-// import CrewsPage from './CrewsPage/CrewsPage';
-// import CrewCard from './CrewsPage/CrewCard';
+import Dashboard from './Dashboard';
+import ClientsPage from './ClientsPage';
+import ContractsPage from './ContractsPage';
+import ContractForm from './ContractForm';
+import JobForm from './JobForm';
+import PackageForm from './PackageForm';
+import Navigation from './Navigation';
+import CrewsPage from './CrewsPage/CrewsPage';
+import CrewCard from './CrewsPage/CrewCard';
 import {} from './dispatchDataHelper';
 
 const Dispatch = function(props) {
+  const crews = props.crews;
+  const clients = props.crews;
+  const packages = props.packages;
+  const contracts = props.contracts;
+  const jobs = props.jobs;
+
   return (
     <div>Dispatch
       <div>Navigation</div>
       <Router >
         <Switch >
-          <Route path="/dispatch/job/:id" >JobForm</Route>
-          <Route path="/dispatch/contract/:id" >EditContractForm</Route>
-          <Route path="/dispatch/contract/new" >NewContractForm</Route>
-          <Route path="/dispatch/package/new" ></Route>
-          <Route path="/dispatch/crews" >BrowseCrews</Route>
-          <Route path="/dispatch/clients" >BrowseClients</Route>
-          <Route path="/dispatch" >Dashboard</Route>
+          <Route path="/dispatch/job/:id" >
+            <JobForm />
+          </Route>
+          <Route path="/dispatch/contract/:id" >
+            <ContractForm />
+            EditContractForm
+          </Route>
+          <Route path="/dispatch/contract/new" >
+            <ContractForm />
+            NewContractForm
+            </Route>
+          <Route path="/dispatch/package/new" >
+            <PackageForm />
+          </Route>
+          <Route path="/dispatch/crews" >
+            <CrewsPage />
+            BrowseCrews
+          </Route>
+          <Route path="/dispatch/clients" >
+            <ClientsPage />
+            BrowseClients
+          </Route>
+          <Route path="/dispatch" >
+            <Dashboard />
+            Dashboard
+          </Route>
         </Switch>
       </Router>
       {/* <Quote /> */}
