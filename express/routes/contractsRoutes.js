6c3b1fc6e24f6e3eb890049f99ddf9038e3b3074
Jonathan Choi <jonchoi11@gmail.com>
2021-09-27 00:33:50 +0000

    add express routes for testing

---
+++ express/routes/contractsRoutes.js
@@ -0,0 +1,26 @@
+const Express = require('express');
+const router = Express.router();
+
+module.exports = (knex) => {
+
+  router.post('/', (req, res) => {
+    const contract = req.body.data;
+
+    knex("contracts")
+    .insert({
+        package_id: contract.package_id,
+        client_id: contract.client_id,
+        address: contract.address,
+        job_notes: contract.job_notes,
+        start_date: contract.start_date
+    })
+    .then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        console.log(`Error: could not POST /contracts ${err}`);
+    });
+  });
+
+  return router;
+};
---
+++ express/routes/packagesRoutes.js
@@ -0,0 +1,28 @@
+const Express = require('express');
+const router = Express.router();
+
+module.exports = (knex) => {
+  router.post('/', (req, res) => {
+    const package = req.body.data;
+
+    knex("packages")
+    .insert({
+      title: package.title,
+      flat_rate: package.flat_rate,
+      size_range_string: package.size_range_string,
+      description: package.description,
+      man_hrs_per_visit: package.man_hrs_per_visit,
+      contract_length_days: package.contract_length_days,
+      visit_interval_days: package.visit_interval_days,
+      package_image: package.image
+    })
+    .then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        console.log(`Error: could not POST /packages ${err}`);
+    });
+  });
+
+  return router;
+};
