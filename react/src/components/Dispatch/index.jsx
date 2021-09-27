56bc07bf316f62bacc85c87f4b2c4e7e63e4b588
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-27 12:37:18 -0600

    add imports to index in crews and dispatch

---
+++ react/src/components/Dispatch/index.jsx
@@ -0,0 +1,17 @@
+
+// import Dashboard from './Dashboard';
+// import ClientsPage from './ClientsPage';
+// import ContractsPage from './ContractsPage';
+// import ContractForm from './ContractForm';
+// import JobForm from './JobForm';
+// import PackageForm from './PackageForm';
+// import Navigation from './Navigation';
+// import CrewsPage from './CrewsPage/CrewsPage';
+// import CrewCard from './CrewsPage/CrewCard';
+import {} from './dispatchDataHelper';
+
+const Dispatch = function(props) {
+
+}
+
+export default Dispatch;
