67a9a5ad6c7d4d8edfeba9c0a8041c21f69371f2
Jonathan Choi <jonchoi11@gmail.com>
2021-09-27 17:19:02 +0000

    add patches for express routes

--- express/routes/contractsRoutes.js
+++ express/routes/contractsRoutes.js
@@ -22,5 +22,25 @@ module.exports = (knex) => {
     });
   });
 
+  router.patch('/:id', (req, res) => {
+    const contract = req.body.data;
+
+    knex("contracts")
+    .where("id", req.params.id)
+    .update("package_id", contract.package_id)
+    .update("client_id", contract.client_id)
+    .update("address", contract.address)
+    .update("job_notes", contract.job_notes)
+    .update("start_date", contract.start_date)
+    .then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        console.log(`Error: could not post /contracts/:${contract.id}`)
+    });
+  });
+
+
+
   return router;
 };
