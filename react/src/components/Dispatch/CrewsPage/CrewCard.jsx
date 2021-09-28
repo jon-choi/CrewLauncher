ac6602822ca15e0e4b162c2f21d1fcf2ff4ba91f
Jonathan Choi <jonchoi11@gmail.com>
2021-09-28 05:02:06 +0000

    format works sort of. still need helper functions for crews

--- react/src/components/Dispatch/CrewsPage/CrewCard.jsx
+++ react/src/components/Dispatch/CrewsPage/CrewCard.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
 
 
 const CrewCard = (props) => {
@@ -10,6 +11,10 @@ const CrewCard = (props) => {
   
   return (
     <Card sx="display: flex; justify-content: center; align-items: flex-start;">
+    <Avatar alt={props.foreman_name} src={props.avatar} sx={{ width: 100, height: 100, mb: 4, ml: 20, mr: 5, mt: 5 }} />
+    {/* <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20, mt: 10 }} />
+    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20, mt: 10 }} /> */}
+
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day
--- react/src/components/Dispatch/CrewsPage/CrewsPage.jsx
+++ react/src/components/Dispatch/CrewsPage/CrewsPage.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
+// import Avatar from '@mui/material/Avatar';
 import Card from '@mui/material/Card';
 // import CardActions from '@mui/material/CardActions';
 // import CardContent from '@mui/material/CardContent';
@@ -13,19 +13,13 @@ const CrewPage = (props) => {
   
   return (
 
-    <Card sx="display: flex; flex-direction: column; justify-content: center;">
-
-    <Avatar alt={props.foreman_name} src={props.avatar} sx={{ width: 100, height: 100, mb: 4, ml: 20, mt: 10 }} />
-    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20 }} />
-    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20 }} />
-
-
-
- {/* CARD ONE */}
-    <CrewCard sx={{ top: 100, position: 'absolute', maxWidth: 200 }}>
+    <Card >
+      
+{/* CARD ONE */}
+    <CrewCard sx={{ top: 100, position: 'absolute', maxWidth: 200, mb: 5 }}>
     </CrewCard>
 
-    <CrewCard sx={{ top: 100, position: 'absolute', maxWidth: 200 }}>
+    <CrewCard sx={{ top: 100, position: 'absolute', maxWidth: 200, mb: 5 }}>
     </CrewCard>
 
     <CrewCard sx={{ top: 100, position: 'absolute', maxWidth: 200 }}>
