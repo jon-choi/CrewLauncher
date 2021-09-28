84e29321b4d9f398c20232ca05a48eb01554cf40
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-27 22:51:55 -0600

    add formatted date and time picker

--- react/src/components/Dispatch/JobForm.jsx
+++ react/src/components/Dispatch/JobForm.jsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router-dom';
 import SpeedDial from '../SpeedDial';
-import Timepicker from '../Timepicker';
-import TextField from '@mui/material/TextField';
+import { TextField, Button } from '@mui/material/';
 import { useState } from 'react';
 import { getInfoForJobForm, getEstTime } from './dispatchDataHelper';
-import { parse, toDate } from 'date-fns'
+import { parse, toDate, format } from 'date-fns'
+import TimePicker from '../Timepicker';
 
 const JobForm = (props) => {
   const params = useParams(props);
@@ -12,37 +12,23 @@ const JobForm = (props) => {
   const [selectedCrew, setSelectedCrew] = useState(0)
   const [time, setTime] = useState(new Date())
   const info = getInfoForJobForm(jobs, contracts, packages, params.id);
-  const estTime = getEstTime(info.packageManHours, crews[selectedCrew])
-  const dateString = toDate(new Date())
-  const date = parse(dateString, 'yyyy', new Date())
-  console.log(date)
+  const estTime = getEstTime(info.packageManHours, (selectedCrew ? crews[selectedCrew - 1] : {crew_size: 1}))
+  const date = format(new Date(), 'EEE MMM dd yyyy')
 
   return (
     <>
       <h1>/dispatch/job/{params.id}</h1>
-      <h3>Date: {info.date}</h3>
+      <h3>Date: {date}</h3>
       <h3>Package: {info.packageTitle}</h3>
       <h3>Address: {info.contractAddress}</h3>
       <h3>Job Notes: {info.contractJobNotes}</h3>
       <h3>Man Hours: {info.packageManHours}</h3>
       {estTime > 1 ? <h3>Estimated Time: {estTime} Hours</h3> : estTime <= 1 && <h3>Estimated Time: {estTime} Hour</h3>}
-
       <div>
-        <TextField
-            required
-            id="outlined-required"
-            label="Required"
-            defaultValue={crews[selectedCrew]}
-        />
-        <TextField
-          id="outlined-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-        />
+        <TimePicker startDate={time} onChange={setTime}/>
       </div>
       <SpeedDial crews={crews} onChange={setSelectedCrew} selectedCrew={selectedCrew}/>
+      <Button variant="contained" onClick={onSave}>Contained</Button>
     </>
   );
 };
