cc061b00b2a4878b8418bd41ed512963cb6f1bc7
Jonathan Choi <jonchoi11@gmail.com>
2021-09-28 03:49:15 +0000

    work on crews page still needed

--- react/src/components/Dispatch/CrewsPage/CrewCard.jsx
+++ react/src/components/Dispatch/CrewsPage/CrewCard.jsx
@@ -1,11 +1,114 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 
-const CrewCard = () => {
+const CrewCard = (props) => {
+
+  const { onClick, date, jobsBooked } = props;
   
   return (
-    <h1>/dispatch/crews
-      Card
-    </h1>
+    <Card sx="display: flex; justify-content: center;">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          something
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+
+      <Card sx="display: flex; justify-content: center;">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          something
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+    </Card>
+
+    <Card sx="display: flex; justify-content: center;">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          something
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+    </Card>
+
+    <Card sx="display: flex; justify-content: center;">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          something
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+    </Card>
+    
+    <Card sx="display: flex; justify-content: center;">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          something
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+    </Card>
+    </Card>
+
+    
+
+    // <h1>/dispatch/crews
+    //   Card
+    // </h1>
   );
 };
 
--- react/src/components/Dispatch/CrewsPage/CrewsPage.jsx
+++ react/src/components/Dispatch/CrewsPage/CrewsPage.jsx
@@ -1,12 +1,35 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+// import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+import CrewCard from './CrewCard';
 
 
-const CrewPage = () => {
+
+const CrewPage = (props) => {
   
   return (
-    <h1>/dispatch/crews
-      Page
-    </h1>
-  );
+
+    <Card direction="column" spacing={10} position="center" top="0" >
+      
+    <Avatar alt={props.foreman_name} src={props.avatar} sx={{ width: 100, height: 100, mb: 4, ml: 20 }} />
+    <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20 }} />
+    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" sx={{ width: 100, height: 100, mb: 4, ml: 20 }} />
+
+
+ {/* CARD ONE */}
+    <CrewCard sx={{ top: 0, position: 'relative', maxWidth: 200 }}>
+    </CrewCard>
+</Card>
+
+    // <h1>/dispatch/crews
+    //   Page Hello
+    // </h1>
+);
 };
 
 export default CrewPage;
