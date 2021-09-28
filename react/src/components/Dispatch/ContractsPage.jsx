bd2d2c9275255b3aa60891aed6c2b2a1a63d3816
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-28 13:20:19 -0600

    fix job seed dates, install date-fns for express

--- react/src/components/Dispatch/ContractsPage.jsx
+++ react/src/components/Dispatch/ContractsPage.jsx
@@ -1,24 +1,35 @@
 import MediaCard from "../MediaCard";
 import {  Link, BrowserRouter as Router, useRouteMatch } from 'react-router-dom';
+import { format } from 'date-fns'
 
 const ContractsPage = (props) => {
   const { contractsInfo, onEdit } = props
   const { url } = useRouteMatch;
   const linkToEdit = <Link to={`${url}/contracts/:id`}></Link>
  
-  const contractCard = contractsInfo.map(contract => {
-    const header = contract
+  const contractCards = contractsInfo.map(contract => {
+    const nextVisit = format(new Date(), 'EEE MMM dd')
+    const header =`${contract.clientName}\n${contract.clientEmail}`
+    const body =
+    `Address: ${contract.address}
+    \nStart Date:${contract.start_date}
+    \nPackage length: ${contract.packageLength}
+    \nJob Notes: ${contract.job_notes}
+    \nNext Visit: ${nextVisit}`
     return <MediaCard
     key={contract.id}
     compClass="contract-card"
     onClick={linkToEdit}
-    header={"text"}
-    body={"text"}
+    header={header}
+    body={body}
     />
   })
 
   return (
+    <>
     <h1>contractInfo: </h1>
+    {contractCards}
+    </>
   );
 };
 
