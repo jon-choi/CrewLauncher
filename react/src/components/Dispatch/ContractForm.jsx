4aef08003c1050488c57299fde2e6607f920b2ed
mcken-vince <mcken.vince@gmail.com>
2021-09-28 11:41:55 -0600

    Show datepicker only when package is selected, props still aren't passing in well

--- react/src/components/Dispatch/ContractForm.jsx
+++ react/src/components/Dispatch/ContractForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MediaCard from '../MediaCard';
 import DateRangePicker from '../DateRangePicker';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Stack';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
@@ -9,7 +10,7 @@ import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import Drawer from '../Drawer';
-import addDays from 'date-fns/addDays';
+import { format, addDays, getDate } from 'date-fns';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -20,14 +21,13 @@ const ContractForm = (props) => {
   const { packages } = props;
   const submit = props.onSubmit;
 
-
   const [error, setError] = useState([]);
   const [selectedPackage, setSelectedPackage] = useState(props.selectedPackage || null);
   const [clientName, setClientName] = useState(props.clientName || "");
   const [clientPhone, setClientPhone] = useState(props.clientPhone || null);
   const [clientEmail, setClientEmail] = useState(props.clientEmail || "");
   const [startDate, setStartDate] = useState(props.startDate || new Date());
-  const [endDate, setEndDate] = useState(addDays(props.startDate, selectedPackage.contract_length_days) || new Date());
+  const [endDate, setEndDate] = useState(props.startDate ? addDays(props.startDate, selectedPackage.contract_length_days) : new Date());
   const [address, setAddress] = useState(props.address || "");
   const [jobNotes, setJobNotes] = useState(props.jobNotes || "");
 
@@ -36,17 +36,23 @@ const ContractForm = (props) => {
   };
 
   const changeDate = (dates) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(addDays(start, selectedPackage.contract_length_days));
-    
+      const [start, end] = dates;
+      setStartDate(start);
+      if (selectedPackage) {
+        setEndDate(addDays(start, selectedPackage.contract_length_days));
+      } else {
+        setEndDate(start);
+      }
   };
 
+  
   const packageCards = packages.map(p => {
-    const packageBody = (`$${p.flat_rate} --
-                    ${p.size_range_string} --
-                    ${p.description} --
-                    Service Interval: ${p.visit_interval_days}-days`)
+    const packageBody = (`
+      $${p.flat_rate} --
+      ${p.contract_length_days} days
+      ${p.size_range_string || ''} --
+      ${p.description} --
+      Service Interval: ${p.visit_interval_days}-days`);
     
     return (
       <Stack spacing={1}>
@@ -60,7 +66,6 @@ const ContractForm = (props) => {
       </Stack>
     )
   });
-  console.log("Selected Package: ", selectedPackage);
     // const [startDate, setStartDate] = useState(new Date());
   // const [endDate, setEndDate] = useState(addDays(startDate, packageLength - 1));
   
@@ -76,24 +81,37 @@ const ContractForm = (props) => {
         
         {error.length > 0 && <Alert severity="error">{`${error.join(', ')} cannot be blank.`}</Alert>}
         
+        <Drawer buttonText={'Select a Package'} items={packageCards} />
+        
+        {selectedPackage &&
+          <>
+            <TextField required disabled label={'Package'} value={selectedPackage ? selectedPackage.title : 'Please Select a Package'} />
+            <DateRangePicker startDate={startDate} endDate={endDate} onChange={changeDate} />
 
-          {/* <OutlinedInput
-            id="package"
-            value={selectedPackage}
-            onChange={event => setSelectedPackage(event.target.value)}
-            label="Please Select a Package"
-          /> */}
-          <Drawer buttonText={'Select a Package'} items={packageCards} />
+            <Box sx={{display: 'flex', 'flex-direction': 'row'}}>
+              <TextField disabled label={'Contract Start'} value={format(startDate, 'MMMM dd, yyyy')} />
+              <TextField disabled label={'Contract End'} value={format(endDate, 'MMMM dd, yyyy')} />
+            </Box>
+          </>
+        }
 
-        <DateRangePicker startDate={startDate} endDate={endDate} onChange={changeDate} />
-
-        <FormControl>
+        <FormControl required>
           <InputLabel htmlFor="clientName">Client Name</InputLabel>
-          <OutlinedInput
+          <OutlinedInput 
             id="clientName"
             value={clientName}
             onChange={event => setClientName(event.target.value)}
             label="Client Name"
+          />
+        </FormControl>
+        
+        <FormControl required>
+          <InputLabel htmlFor="clientEmail">Client Email</InputLabel>
+          <OutlinedInput
+            id="clientEmail"
+            value={clientEmail}
+            onChange={event => setClientEmail(event.target.value)}
+            label="Client Email"
           />
         </FormControl>
         
@@ -107,17 +125,7 @@ const ContractForm = (props) => {
           />
         </FormControl>
         
-        <FormControl>
-          <InputLabel htmlFor="clientEmail">Client Email</InputLabel>
-          <OutlinedInput
-            id="clientEmail"
-            value={clientEmail}
-            onChange={event => setClientEmail(event.target.value)}
-            label="Client Email"
-          />
-        </FormControl>
-        
-        <FormControl>
+        <FormControl required>
           <InputLabel htmlFor="address">Address</InputLabel>
           <OutlinedInput
             id="address"
@@ -136,7 +144,7 @@ const ContractForm = (props) => {
           onChange={event => setJobNotes(event.target.value)}
         />
         
-        <Button onClick={validate} variant="contained">Create New Package</Button>
+        <Button onClick={validate} variant="contained">Create New Contract</Button>
       </Stack>
     </>
   );
