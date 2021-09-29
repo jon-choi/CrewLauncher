59c8c8f17cdd1dad50b81bf81f94b58f041501aa
Jonathan Choi <jonchoi11@gmail.com>
2021-09-29 01:22:11 +0000

    starting from scratch

--- react/src/components/Crews/index.jsx
+++ react/src/components/Crews/index.jsx
@@ -4,16 +4,14 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import Navigation from './Navigation';
 import Day from './Day';
 import Jobs from './Jobs';
-import { getJobsByCrew } from './crewsDataHelper';
+import {} from './crewsDataHelper';
 
 const Crews = function(props) {
   const { url } = useRouteMatch();
 
   const days = props.days;
   const jobs = props.jobs;
-  const crews = props.crews;
 
-  const jobsByCrew = getJobsByCrew(jobs, crews)
 
   return (
   <div>
