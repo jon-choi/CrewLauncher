9bf903f9b240b069f53b6bd8e0aff74560928b0d
mcken-vince <mcken.vince@gmail.com>
2021-09-29 17:32:30 -0600

    Client card/page layout

--- react/src/components/Dispatch/ClientsPage.jsx
+++ react/src/components/Dispatch/ClientsPage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Stack, Box } from '@mui/material';
+import { Stack } from '@mui/material';
 import ClientCard from './ClientCard';
 
 const ClientsPage = (props) => {
     console.log("clientsInfo: ", props.clientsInfo)
 
   const clientCards = props.clientsInfo.map(client => {
-    return (<ClientCard client={client} />)
+    return (<ClientCard key={client.id} client={client} />)
   });
 
   return (
     <>
-    <div>Total Clients: {props.clientsInfo.length}</div>
-    <Stack spacing={1} >
+      <div>Total Clients: {props.clientsInfo.length}</div>
+      <Stack spacing={1} >
         {clientCards}
-    </Stack>
+      </Stack>
     </>
   );
 };
