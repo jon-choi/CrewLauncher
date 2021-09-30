cb4a7fd853d68d075eabf49c5f0f7aaa50929f7d
Jonathan Choi <jonchoi11@gmail.com>
2021-09-30 05:21:18 +0000

    attempt to use state in crews index. create hook file

--- react/src/components/Crews/index.jsx
+++ react/src/components/Crews/index.jsx
@@ -6,6 +6,9 @@ import Jobs from './Jobs';
 import Quote from './Quote';
 import { getJobsByCrew, getJobsByCrewByDay } from './crewsDataHelper';
 import { getDayInfo } from '../../helpers/AppHelpers';
+import { Stack, Typography, Box } from '@mui/material';
+import JobCard from '../JobCard';
+
 
 const Crews = function(props) {
   const { onSubmitQuote } = props;
@@ -13,28 +16,41 @@ const Crews = function(props) {
   const params = useParams();
  
   // const jobsByCrewByDay = getJobsByCrewByDay(jobs, crews);
-  const { jobs, crews, contracts, packages, clients } = props
-  const jobsByCrew = getJobsByCrew(jobs, crews);
-
+  
+ 
+const { jobs, crews, contracts, packages, clients } = props
+   const jobsByCrew = getJobsByCrew(jobs, crews);
  
   const days = getDayInfo(jobs, crews, contracts, packages, clients, parseInt(params.id));
   
-  const [selectedDay, setSelectedDay] = useState([{}])
-  
+  const [selectedDay, newDayCards] = useState([{}])
   
   if (days) {
-    const [yesterday, today, tomorrow, fourthDay, lastDay] = days;
-    const date = setSelectedDay(days);
+    const jobCard = days.map(jobOfDay => {
+      const { job, contractOfJob, crewOfJob, packageOfJob, clientOfJob } = jobOfDay;
     
-    const {job, contractOfJob, crewOfJob, packageOfJob, clientOfJob } = yesterday[1]
 
     return (
       <div>
     
         <Navigation packages={props.packages} onSubmitQuote={onSubmitQuote}/>
           <Switch >
+          <Stack>
+            <Typography variant="h6">{clientOfJob.name}</Typography>
+            <Box sx={{ width: '95%', maxWidth: 500, maxHeight: 300, display: 'flex'}}>
+              <JobCard
+              key={job.id}
+              packageTitle={packageOfJob.title}
+              timeEst={packageOfJob.man_hours_per_visit}
+              clientName={clientOfJob.name}
+              address={contractOfJob.address}
+              jobNotes={contractOfJob.job_notes}
+              compClass="dashboard-day"
+              />
+            </Box>
+          </Stack>
             <Route path="/crews/:id/days/:day">
-              <Day days={getDayInfo} selectedDay={selectedDay} />
+              <Day days={getDayInfo} selectedDay={selectedDay} {...jobCard} />
             </Route>
     
             <Route path="/crews/:id/jobs">
@@ -51,6 +67,7 @@ const Crews = function(props) {
     
       </div>
       );
+    })
   }
   return (
     <>
