1e845d68576afadb8e334460241439d593c10155
mcken-vince <mcken.vince@gmail.com>
2021-10-01 12:04:14 -0600

    Create dispatch quotes drawer component

---
+++ react/src/components/Dispatch/Navigation/NavigationQuotes.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { Drawer, Button, Divider, Stack, Card, Typography, CardActions } from '@mui/material';
+const NavigationQuotes = (props) => {
+  const { open, setOpen, quotes } = props;
+
+  const quoteCards = quotes.map(quote => {
+    return (
+    <Card key={quote.clientEmail}>
+      <CardActions>
+        <Button component={Link} to={`/dispatch/contracts/new`}>Handle Quote</Button>
+      </CardActions>
+      <Typography>
+        {quote.clientName}
+      </Typography>
+      <Typography>
+        {quote.clientEmail}
+      </Typography>
+      <Typography>
+        {quote.clientPhone}
+      </Typography>
+      <Typography>
+        Requested Package: {quote.selectedPackage}
+      </Typography>
+    </Card>);
+  });
+
+
+  return (
+    <Drawer open={open} variant='persistent' position='static' anchor='left'
+      sx={{display: { xs: 'block', sm: 'block' },
+      '& .MuiDrawer-paper': { boxSizing: 'border-box', width: props.drawerWidth },
+    }}>
+      <Button><h1 onClick={()=> setOpen(false)}>Crew🚀Launcher</h1></Button>
+      <Divider />
+      <Stack>
+        {quoteCards}
+      </Stack>
+    </Drawer>
+  )
+}
+
+export default NavigationQuotes;
