07a05484a0985493d71b12989280f68e95958a9a
mcken-vince <mcken.vince@gmail.com>
2021-10-01 11:13:46 -0600

    Remove lone const

--- react/src/components/Dispatch/Navigation/NavigationEmptyJob.jsx
+++ react/src/components/Dispatch/Navigation/NavigationEmptyJob.jsx
@@ -2,7 +2,7 @@ import {  Link,NavLink, useRouteMatch } from 'react-router-dom';
 import {  Toolbar, Drawer, MenuList, MenuItem, AppBar, Box, Divider, CssBaseline } from '@mui/material';
 
 const NavigationEmptyJob = function(props) {
-  const
+
 };
 
 export default NavigationEmptyJob;
