d3ea63bae6f73caad3f550126d3d0387b86d4623
GledfullyChill <stephan.lofstrom@gmail.com>
2021-10-01 18:41:53 -0600

    add crew day

--- react/src/components/Crews/CrewDashboard.jsx
+++ react/src/components/Crews/CrewDashboard.jsx
@@ -1,18 +1,29 @@
-import Stack from '@mui/material/Stack';
+
 import useDayInfo from './hooks/CrewsIndexHook';
+import { Stack, Box, Grid, Fab } from '@mui/material';
 
 
 const CrewDashboard = function(props) {
-  const { selectedDay, newDayCards } = useDayInfo()
+  const { selectedDay, setSelectedDay, newDayCards } = useDayInfo()
   const { days } = props;
   
   if (days) {
+    const fab = (<Fab variant="extended"
+    onClick={() => {
+      setSelectedDay(null)
+
+      }} sx={{
+        position: 'sticky',
+        top: 16,
+        left: 850,
+      }}>Close</Fab>
+    );
 
     
     // const [yesterday, today, tomorrow, fourthDay, lastDay] = days;
 
     
-    const dayCards = newDayCards(days);
+    const dayCards = newDayCards(days, fab);
 
     return (
       <Stack sx={{margin: 'auto', width: '100%'}} >
