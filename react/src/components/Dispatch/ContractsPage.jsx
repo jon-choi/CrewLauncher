33a6f705d75c09560804832a5656c808ba9c2a92
mcken-vince <mcken.vince@gmail.com>
2021-10-02 15:11:45 -0600

    Add contract progress circle with % value inside

--- react/src/components/Dispatch/ContractsPage.jsx
+++ react/src/components/Dispatch/ContractsPage.jsx
@@ -1,6 +1,6 @@
 import MediaCard from "../MediaCard";
-import { format } from 'date-fns'
-import { Stack, Box, Typography, Grid } from '@mui/material';
+import { format, isWithinInterval, addDays, differenceInDays } from 'date-fns'
+import { Stack, Box, Typography, Grid, CircularProgress } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
@@ -21,16 +21,44 @@ const ContractsPage = (props) => {
       🚀 {contract.clientName} 🚀{contract.clientEmail} 
       </Typography>
     </Item>);
-    const startDate =  format(new Date(contract.start_date), 'EEE MMM d yyyy')
-    const body = (<Stack container>
-      <Item>Address: ${contract.address}</Item>
+    
+    const startDate = format(new Date(contract.start_date), 'EEE MMM d yyyy');
+    const endDate = new Date(addDays(new Date(startDate), parseInt(contract.packageLength)));
+    const currentContract = isWithinInterval(new Date(), {start: new Date(startDate), end: endDate});
+    const daysLeftInContract = differenceInDays(endDate, new Date());
+    const contractProgress = Math.round((contract.packageLength - daysLeftInContract) / contract.packageLength * 100);
+
+    const body = (
+    <Stack container>
+      {currentContract && contractProgress && 
+      
+      <Box sx={{position: 'relative', display: 'inline', border: '2px red solid'}}>
+        <CircularProgress variant="determinate" value={contractProgress} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="caption" component="div" color="text.secondary">
+            {`${contractProgress}%`}
+          </Typography>
+        </Box>
+      </Box>
+      }
+      <Item>Address: {contract.address}</Item>
       <Item>Start Date:{startDate}</Item>
       <Item>Job Notes: {contract.job_notes}</Item>
       <Item>Package length: {contract.packageLength}</Item>
       <Item>Next Visit: {nextVisit}<Typography variant="h6">🚀</Typography></Item>
       </Stack>);
     const linkToEdit = `/dispatch/contracts/${contract.id}`;
-    
 
     return <MediaCard
     key={count}
