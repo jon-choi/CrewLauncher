6181f7a4a93d6b214bd1d5d08762c951e0ec5069
mcken-vince <mcken.vince@gmail.com>
2021-10-04 21:08:22 -0600

    Working on crews navbar; quote drawer is not opening even though value is set to true

--- react/src/components/Crews/Navigation.jsx
+++ react/src/components/Crews/Navigation.jsx
@@ -1,37 +1,52 @@
-
+import { useState } from 'react';
 import { useParams, Link } from 'react-router-dom'
 import Drawer from '../Drawer'
 import Quote from './Quote'
-import Card from '@mui/material/Card';
-import { Stack, Box, FormControl, InputLabel, OutlinedInput, TextField, Alert, Button, Snackbar, Typography, Avatar } from '@mui/material';
+import { Box, Button, IconButton, AppBar, Toolbar } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 
 const Navigation = (props) => {
-
+  const [drawerOpen, setDrawerOpen] = useState(true);
   const id = useParams().id;
 
   const { packages, onSubmitQuote } = props;
+  
   const Item = styled(Paper)(({ theme }) => ({
       ...theme.typography.body2,
       padding: theme.spacing(1),
       textAlign: 'center'
   }));
 
-  return (
-    <div>
-      <Stack direction="row" sx={{justifyContent: 'space-between', minWidth: '350px', maxWidth: '350px'}}>
-        <Button component={Link} to={`/crews/${id}`} sx={{fontSize:100}}>
-        🚀
-        </Button>
-      
-        <Item>
-          <Drawer closeButtonText={'Close'} openButtonText={'Quote'} items={<Quote packages={packages} onSubmitQuote={onSubmitQuote} width={'192vw'} />} />
-        </Item>
-      </Stack>
-    </div>
-  );
+
+  return (<>
+    <AppBar position='fixed' width='100%'>
+      <Toolbar width='100%' >
+        <IconButton size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+          component={Link} 
+          to={`/crews/${id}`} 
+        >🚀</IconButton>
+        <Button variant='contained' color='inherit' onClick={() => setDrawerOpen(true)}>Quote</Button>
+      </Toolbar>
+    </AppBar>
+    <Item>
+      <Drawer
+        anchor={'right'}
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+      <Box sx={{ width: '192vw' }} role="presentation">
+          <Button onClick={() => setDrawerOpen(false)} variant="contained">Close</Button>
+          <Quote packages={packages} onSubmitQuote={onSubmitQuote} />
+        </Box>
+      </Drawer>
+    </Item>
+  </>);
 };
 
 export default Navigation;
+
--- react/src/components/Drawer.jsx
+++ react/src/components/Drawer.jsx
@@ -38,27 +38,7 @@ export default function NewDrawer(props) {
     >
       <Button onClick={toggleDrawer(anchor, false)} variant="contained">{closeButtonText}</Button>
       {items}
-      {/* <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List> */}
+      
     </Box>
   );
 
